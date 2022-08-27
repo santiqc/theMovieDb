97914e1983c0e38acb1d06bff67ef237a84fec6d
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2022-08-27 18:12:30 -0500

    se agrego angular material
    Se mejoro la búsqueda por nombre

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
-
+  handleSearch(value: string){
+    if(value && value.length > 3) {
+      this.router.navigate(['/search-movie'],{queryParams: {query: value}});
+    }
+  }
+  
 }
