e90172a33a17eefadcb2094aea33d2b3f6643960
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2022-08-26 20:37:22 -0500

    detail movie

--- src/app/Entities/movies/detail-movie/detail-movie.component.ts
+++ src/app/Entities/movies/detail-movie/detail-movie.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { DetailMovie } from '../detail-movie';
+import { Movie } from '../movie';
+import { MovieService } from '../service/movie.service';
 
 @Component({
   selector: 'app-detail-movie',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-movie.component.css']
 })
 export class DetailMovieComponent implements OnInit {
-
-  constructor() { }
+  url= environment.seeImg;
+  movie: Movie[];
+  detailMovie: DetailMovie;
+  constructor(
+    private movieService: MovieService,
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.movieService.getDetailMovie(id).subscribe(data => {
+        this.detailMovie = data;
+        console.log(this.detailMovie);
+      }
+    );
   }
 
 }
