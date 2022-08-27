1ac1b7c674120a0d93c300281d0578c20271c595
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2022-08-26 21:48:41 -0500

    se agrego spiner

--- src/app/Entities/movies/detail-movie/detail-movie.component.ts
+++ src/app/Entities/movies/detail-movie/detail-movie.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { environment } from 'src/environments/environment.prod';
 import { DetailMovie } from '../detail-movie';
 import { Movie } from '../movie';
@@ -17,15 +18,25 @@ export class DetailMovieComponent implements OnInit {
   constructor(
     private movieService: MovieService,
     private activatedRoute: ActivatedRoute,
+    private spinner: NgxSpinnerService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.spinner.show();
     const id = this.activatedRoute.snapshot.params['id'];
     this.movieService.getDetailMovie(id).subscribe(data => {
+      this.spinner.hide();
         this.detailMovie = data;
         console.log(this.detailMovie);
+      },
+      err => {
+        this.spinner.hide();
+        console.log(err);
       }
     );
   }
 
+  goBack(): void {
+    this.router.navigate(['/']);}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,22 @@ import { NavbarComponent } from './Layouts/navbar/navbar.component';
 import { ListMovieComponent } from './Entities/movies/list-movie/list-movie.component';
 import { DetailMovieComponent } from './Entities/movies/detail-movie/detail-movie.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Import library module
+import { NgxSpinnerModule } from "ngx-spinner";
+import {
+  SwiperModule, SwiperConfigInterface,
+  SWIPER_CONFIG
+} from 'ngx-swiper-wrapper';
 
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  observer: true,
+  direction: 'horizontal',
+  threshold: 50,
+  spaceBetween: 5,
+  slidesPerView: 1,
+  centeredSlides: true
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,9 +39,18 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgxSpinnerModule,
+    SwiperModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  entryComponents: [DetailMovieComponent],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
