1ad4241081767338ddbdd6b04f21f1a21792aa9b
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2022-08-26 19:52:21 -0500

    Creación de carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailMovieComponent } from './Entities/movies/detail-movie/detail-movie.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent
+  },
+  {
+    path: 'detail/:id',
+    component: DetailMovieComponent
+  },
+  {
+    path: '**', 
+    redirectTo: '',
+    pathMatch: 'full'
+  } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ListMovieComponent } from './Entities/movies/list-movie/list-movie.component';
+import { MovieServiceComponent } from './Entities/movies/service/movie-service/movie-service.component';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
+import { HttpClientModule  } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './Layouts/navbar/navbar.component';
+import { DetailMovieComponent } from './Entities/movies/detail-movie/detail-movie.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListMovieComponent,
+    DetailMovieComponent,
+    MovieServiceComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
